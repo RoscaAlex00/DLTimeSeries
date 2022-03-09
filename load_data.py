eedd60607e4de615d8da3a99fab089e75297c0b2
Alexei <56193131+RoscaAlex00@users.noreply.github.com>
2022-03-09 17:42:35 +0100

    first VAR model results

--- load_data.py
+++ load_data.py
@@ -8,26 +8,34 @@ from IPython.display import display
 def load_alcohol():
     path_train = 'data/alcohol_data/train/*.csv'
     path_test = 'data/alcohol_data/test/*.csv'
+    path_raw = 'data/alcohol_data/raw/*.csv'
     CURRENT_DIR = os.path.dirname(__file__)
     file_train = glob.glob(os.path.join(CURRENT_DIR, path_train))
     file_test = glob.glob(os.path.join(CURRENT_DIR, path_test))
+    file_raw = glob.glob(os.path.join(CURRENT_DIR, path_raw))
 
-    df_train = (pd.read_csv(file) for file in file_train)
-    df_test = (pd.read_csv(file) for file in file_test)
+    df_train = [pd.read_csv(file) for file in file_train]
+    df_test = [pd.read_csv(file) for file in file_test]
+    list_raw = [pd.read_csv(file) for file in file_raw]
 
     train_df = pd.concat(df_train, ignore_index=True)
     test_df = pd.concat(df_test, ignore_index=True)
+
     train_df.drop(train_df.columns[0], axis=1, inplace=True)
     test_df.drop(test_df.columns[0], axis=1, inplace=True)
 
-    return train_df, test_df
+    return train_df, test_df, list_raw
 
 
 def prepare_data_alcohol(df_patient):
-    return -1
+    replacement_names = ['start', 'finish', 'drinks', 'comfortable', 'stressed', 'down', 'calm', 'pressure',
+                         'enthusiastic', 'happy', 'conflict', 'craving', 'impulsive', 'pos_expect', 'peer_percent',
+                         'want_drink', 'delay_grat', 'angry', 'drink_predict', 'restless_sleep', 'difficulty_sleep',
+                         'hours_sleep']
 
+    df_patient.columns = replacement_names
+    df_patient = df_patient.set_index('start')
+    df_patient = df_patient.loc[df_patient['finish'].notnull()]
 
-train, test = load_alcohol()
+    return df_patient
 
-print(train.columns.values.tolist())
-print(train.head(50))

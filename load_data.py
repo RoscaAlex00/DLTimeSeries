6b422a98a90cd5a74396c072cafe1967f5298676
Alexei <56193131+RoscaAlex00@users.noreply.github.com>
2022-04-04 16:09:45 +0200

    add jupyter notebook

--- load_data.py
+++ load_data.py
@@ -18,24 +18,29 @@ def load_alcohol():
     df_test = [pd.read_csv(file) for file in file_test]
     list_raw = [pd.read_csv(file) for file in file_raw]
 
-    train_df = pd.concat(df_train, ignore_index=True)
-    test_df = pd.concat(df_test, ignore_index=True)
+    # Concat for global model
+    # train_df = pd.concat(df_train, ignore_index=True)
+    # test_df = pd.concat(df_test, ignore_index=True)
 
-    train_df.drop(train_df.columns[0], axis=1, inplace=True)
-    test_df.drop(test_df.columns[0], axis=1, inplace=True)
+    # train_df.drop(train_df.columns[0], axis=1, inplace=True)
+    # test_df.drop(test_df.columns[0], axis=1, inplace=True)
 
-    return train_df, test_df, list_raw
+    return df_train, df_test, list_raw
 
 
 def prepare_data_alcohol(df_patient):
+    if len(df_patient.columns) > 22:
+        df_patient = df_patient.drop(columns=['Since the last survey; how many times have you used a tobacco product?'])
     replacement_names = ['start', 'finish', 'drinks', 'comfortable', 'stressed', 'down', 'calm', 'pressure',
                          'enthusiastic', 'happy', 'conflict', 'craving', 'impulsive', 'pos_expect', 'peer_percent',
                          'want_drink', 'delay_grat', 'angry', 'drink_predict', 'restless_sleep', 'difficulty_sleep',
                          'hours_sleep']
 
     df_patient.columns = replacement_names
+    df_patient['start'] = pd.to_datetime(df_patient['start'])
     df_patient = df_patient.set_index('start')
     df_patient = df_patient.loc[df_patient['finish'].notnull()]
+    df_patient['finish'] = pd.to_datetime(df_patient['finish'])
+
 
     return df_patient
-
